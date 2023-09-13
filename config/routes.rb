482e624df582ff84c5f601dfb23ec71379648ccf
Beatriz Vieira <beaferreirav@gmail.com>
2023-09-13 17:03:04 +0100

    new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/pages/new', to: 'pages#new', as: 'form'   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
